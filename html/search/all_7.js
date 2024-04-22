42375dad90c4255d318dbdb99012596f2932c3d6
alexei-02 <94101650+alexei-02@users.noreply.github.com>
2024-04-22 19:52:51 +0300

    [lab-tsc] done

---
+++ html/search/all_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['testtree_0',['TestTree',['../classtest__tree_1_1_test_tree.html',1,'test_tree']]],
+  ['traversal_1',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['tree_2',['Tree',['../classtree_1_1_tree.html',1,'tree']]],
+  ['tree_20traversal_3',['Tree traversal',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/classes_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['testtree_0',['TestTree',['../classtest__tree_1_1_test_tree.html',1,'test_tree']]],
+  ['tree_1',['Tree',['../classtree_1_1_tree.html',1,'tree']]]
+];
